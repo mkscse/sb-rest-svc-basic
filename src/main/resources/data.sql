34e46c33f853e89f02b979d526d73ab42a14da63
mkscse <mks.cse@outlook.com>
2024-03-16 14:53:32 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into student values(10001,'Ranga', 'E1234567');
+insert into student values(10001,'Mukesh', 'E1234567');
 
-insert into student values(10002,'Ravi', 'A1234568');
+insert into student values(10002,'Amit', 'A1234568');
